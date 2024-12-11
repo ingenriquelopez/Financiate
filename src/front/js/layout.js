3d891494aa4f0f9891c0ec1cd0b9fde3bf8e13b3
Enrique Lopez <morelearning2025@gmail.com>
2024-12-11 18:10:28 +0000

    struct and login 01

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,17 @@
+// import { Demo } from "./pages/demo";
+// import { Single } from "./pages/single";
+// import { Navbar } from "./component/navbar";
+
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import Login from './component/Login/Login.jsx';
 
 //create your first component
 const Layout = () => {
@@ -23,11 +25,12 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    {/* <Navbar /> */}
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login/>} path="/" />
+                        <Route element={<Home />} path="/Home" />
+                        {/* <Route element={<Demo />} path="/demo" />
+                        <Route element={<Single />} path="/single/:theid" /> */}
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
