cc997171001a8c6bae6736e4428b27ab48b6be30
Enrique Lopez <morelearning2025@gmail.com>
2024-12-11 01:35:40 +0000

    holi3

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-/*holi2*/
+/*holi3*/
 import { Context } from "../store/appContext";
 
 export const Demo = () => {
