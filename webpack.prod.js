e189cf1a5c8c0e0842a31a4da5a9cc067e9e153e
Enrique Lopez <95664476+ingenriquelopez@users.noreply.github.com>
2025-01-18 19:59:16 +0000

    incuir archivos de produccion de front

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path'); // Necesitamos esto para resolver la ruta
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'dist'), // Aquí se define la carpeta de salida
+        filename: 'bundle.js',
         publicPath: '/'
     },
     plugins: [
