5446ffd97ecb44ebb705a4fd24a38cd5099037b2
Enrique Lopez <95664476+ingenriquelopez@users.noreply.github.com>
2025-01-18 22:37:32 +0000

    app4.py

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ from api.commands import setup_commands
 from flask_cors import CORS
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname()
- static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../dist/')
+static_file_dir = os.path.join(os.path.dirname())
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../dist/')
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
